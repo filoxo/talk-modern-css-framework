08bc17e0d0ab3476c9f725f47210283afd0f2915
Carlos Filoteo <c.filoteo@live.com>
2017-09-18 22:19:53 -0600

    Add Ideas, end slide

--- src/presentation.js
+++ src/presentation.js
@@ -11,7 +11,8 @@ import {
   List,
   Quote,
   Slide,
-  Text
+  Text,
+  Notes
 } from 'spectacle'
 
 // Import theme
@@ -44,11 +45,31 @@ export default class Presentation extends React.Component {
           <Heading size={1} lineHeight={1.3} textColor="heading">
             Ideas
           </Heading>
-          <List>
-            <ListItem>asdf</ListItem>
+          <List style={{ width: '50%', margin: '0 auto' }}>
+            <ListItem>Grey-scale</ListItem>
+            <ListItem>Themable</ListItem>
+            <ListItem>
+              <a href="https://medium.com/@matuzo/writing-css-with-accessibility-in-mind-8514a0007939">
+                Focus on Accessibility
+              </a>
+            </ListItem>
+            <Notes>
+              <p>Here are some ideas for you to go forth and try:</p>
+              <ul>
+                <li>A theme that uses only grey-scale colors</li>
+                <li>
+                  A framework that is themable using CSS custom properties
+                </li>
+                <li>Dive deep into enhancing accessibility with CSS</li>
+              </ul>
+            </Notes>
           </List>
         </Slide>
-        <Slide transition={['fade']} />
+        <Slide transition={['fade']}>
+          <Heading size={2} textColor="highlight">
+            Thanks!
+          </Heading>
+        </Slide>
       </Deck>
     )
   }
