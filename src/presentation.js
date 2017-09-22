4e423afc50a3d3bc9dbb5d2c910633c3edc85140
Carlos Filoteo <c.filoteo@live.com>
2017-09-21 22:30:57 -0600

    Add 'Why npm scripts' slide and notes

--- src/presentation.js
+++ src/presentation.js
@@ -98,6 +98,30 @@ export default class Presentation extends React.Component {
           </Notes>
         </Slide>
         <Slide transition={['fade']}>
+          <Heading size={2} textColor="heading">
+            So why npm scripts?
+          </Heading>
+          <List style={{ width: '50%', margin: '0 auto' }}>
+            <ListItem>Small tasks</ListItem>
+            <ListItem>Composability</ListItem>
+            <ListItem>Abstracted configuration</ListItem>
+          </List>
+          <Notes>
+            <p>
+              So why use npm scripts as the build tool? If you're not familiar
+              with npm, it is the largest software registry and isn't limited to
+              JavaScript. On your machine, it will help you manage your projects
+              dependencies (like Bower), define executable scripts (like Grunt
+              and Gulp), and manage settings and metadata.
+            </p>
+            <p>
+              Using npm has simplified my development process because it
+              replaces 2 other tools. That means less things to install and
+              configure. npm's ubiquity is also a plus.
+            </p>
+          </Notes>
+        </Slide>
+        <Slide transition={['fade']}>
           <Heading size={1} lineHeight={1.3} textColor="heading">
             Ideas
           </Heading>
