7fe9bebf06ab83fa9400075f4e0c79733d5a5031
Carlos Filoteo <c.filoteo@live.com>
2017-10-03 08:03:25 -0600

    Add TodoPt3 for later

---
+++ src/slides/TodoPt3.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Slide, Heading, List, ListItem, CodePane, Code } from 'spectacle'
+
+export default (
+  <Slide transition={['fade']}>
+    <div style={{ maxWidth: '100vw' }}>
+      <Heading size={3} textColor="highlight">
+        TODO, pt. 3
+      </Heading>
+      <List ordered style={{ textAlign: 'left', marginBottom: 0 }}>
+        <ListItem>
+          Create <Code>index.html</Code>
+        </ListItem>
+        <ListItem>Populate with content</ListItem>
+      </List>
+      <CodePane
+        fit
+        lang="html"
+        className="language-html"
+        style={{ fontSize: '3.5vmin' }}
+      >
+        {`...
+<link rel="stylesheet" href="dist/main.min.css">
+...   
+<section>
+  <h2>Buttons</h2>
+  <div>
+    <button class="primary" type="button">Primary</button>
+    <button class="secondary" type="button">Secondary</button>
+  </div>
+</section>`}
+      </CodePane>
+    </div>
+  </Slide>
+)
