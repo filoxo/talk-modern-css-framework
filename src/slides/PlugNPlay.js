d74ed3a4a7204d417bf37e871735bffb4be34b4c
Carlos Filoteo <c.filoteo@live.com>
2017-10-07 16:59:57 -0600

    Add notes

--- src/slides/PlugNPlay.js
+++ src/slides/PlugNPlay.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Slide } from 'spectacle'
+import { Slide, Notes } from 'spectacle'
 
 export default (
   <Slide bgImage="./LEGO-Loop.gif">
     <div style={{ color: '#666', marginTop: '85vh', textAlign: 'right' }}>
       Credit: Chris Gannon
     </div>
+    <Notes>
+      So npm is just one piece of this build pipeline, the backbone piece, and
+      along the way you can choose to add, remove, or replace anything. This is
+      the power of building your own framework; it gives you total control.
+    </Notes>
   </Slide>
 )
