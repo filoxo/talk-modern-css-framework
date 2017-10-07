460ea58c7f8f0a0cb121bad06b9e65ab1d6a8067
Carlos Filoteo <c.filoteo@live.com>
2017-10-07 14:27:51 -0600

    Add AncestryDNA logo, notes

--- src/slides/AboutMe.js
+++ src/slides/AboutMe.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Slide, Heading, Text, Layout, Fit, Fill } from 'spectacle'
+import { Slide, Heading, Text, Layout, Fit, Fill, Notes } from 'spectacle'
 import twitterSvg from '../twitter.svg'
 
 export default (
@@ -21,7 +21,14 @@ export default (
         />
       </Fit>
       <Fill style={{ marginTop: '3vmin' }}>
-        <Text textAlign="left">Software Engineer, AncestryDNA</Text>
+        <Text textAlign="left">
+          <img
+            src="https://www.ancestrycdn.com/ui-static/i/logo/ancestry-dna-on-dark.svg"
+            alt="AncestryDNA logo"
+            style={{ height: '2.5rem', marginBottom: -10 }}
+          />
+        </Text>
+        <Text textAlign="left">Front-end Developer</Text>
         <Text textAlign="left">
           <img
             src={twitterSvg}
@@ -33,5 +40,19 @@ export default (
         </Text>
       </Fill>
     </Layout>
+    <Notes>
+      A little about me: my name is Carlos Filoteo, originally from Las Vegas,
+      NV. I currently work as a front-end developer for AncestryDNA in Lehi UT.
+      You can reach out to me on Twitter after this talk if you have any
+      questions.
+      <p>
+        I'm experiencing a lot of firsts this weekend.
+        <ul>
+          <li>This is my first time in New Orleans</li>
+          <li>first time attending @CSSDevConf</li>
+          <li>first time speaking in front of such a large audience</li>
+        </ul>
+      </p>
+    </Notes>
   </Slide>
 )
