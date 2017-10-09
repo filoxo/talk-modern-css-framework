daff438739969a1a32d71faf9f2302f54c3934c5
Carlos Filoteo <c.filoteo@live.com>
2017-10-09 11:57:42 -0500

    Add feedback text

--- src/slides/Thanks.js
+++ src/slides/Thanks.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Slide, Heading } from 'spectacle'
+import { Slide, Heading, Text } from 'spectacle'
 
 export default (
   <Slide transition={['fade']}>
     <Heading size={2} textColor="highlight">
       Thanks!
     </Heading>
+    <Heading size={4} textColor="heading">
+      #npmcss [1-5] feedback & comments
+    </Heading>
+    <Text>504 299 6828</Text>
   </Slide>
 )
